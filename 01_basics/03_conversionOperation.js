a4dee501bc4350119db0c26f4a618c4f94e073d6
PiyushAgarwal23 <91032431+PiyushAgarwal23@users.noreply.github.com>
2024-09-26 18:24:37 +0000

    conversions of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,24 @@
+let score ="false"
+console.log(typeof score);
+let valueInNumber = Number(score) //Number = use to convert the datatype to number.
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+
+//"33" => 33
+//"33abc" =>Nan
+//true => 1; false =>0;
+
+
+let isLoggedIn= ""
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+//1=> true; 0=> false
+//""=> false
+//"piyus"=>true
+
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(typeof stringNumber);
